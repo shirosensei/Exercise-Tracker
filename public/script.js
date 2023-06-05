4eee71d550a1527232ec7ab7e47e6ab7bd8b70b4
Robert <63929802+cashmerefrosh911@users.noreply.github.com>
2023-06-05 19:31:46 -0400

    Added fluid responsivenes

--- public/script.js
+++ public/script.js
@@ -1,20 +1,32 @@
-var images = [
-    'image1.jpg', 
-    'image2.jpg',
-    'image3.jpg',
-    'image4.png',
-    'image5.jpg'
+// var images = [
+//     'image1.jpg', 
+//     'image2.jpg',
+//     'image3.jpg',
+//     'image4.png',
+//     'image5.jpg'
   
-  ];
-var index = 0;
-var slideshowImage = document.querySelector('.slideshow-image');
+//   ];
+// var index = 0;
+// var slideshowImage = document.querySelector('.slideshow-image');
 
-function changeBackground() {
-  slideshowImage.style.backgroundImage = `url('${images[index]}')`;
-  index = (index + 1) % images.length;
-}
+// function changeBackground() {
+//   slideshowImage.style.backgroundImage = `url('${images[index]}')`;
+//   index = (index + 1) % images.length;
+// }
 
-setInterval(changeBackground, 2000);
+// setInterval(changeBackground, 2000);
 
 let hamburger = document.querySelector('.hamburger');
 let navLinks = document.getElementById('nav-links');
+let links = document.querySelectorAll('.links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('hide');
+  hamburger.classList.toggle('line-rotate')
+})
+
+for(let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', () => {
+    navLinks.classList.toggle('hide');
+  });
+}
